90026f67409c85e7eccd9c3e24a8a8a1f742d7d7
jmgerow <justingerow1@gmail.com>
2018-07-04 15:18:39 -0500

    fixed animation and reorganized functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,134 +1,99 @@
+$( document ).ready(function() {
+// array to hold search buttons for user
+var searchButtons = ["husky","westworld","monkey","the office","dogs","subaru","bird","GOT","movies"];
 
+// function to create user buttons
+function gifButtons() {
 
+    $("#gifSelector").empty();
 
-var search = "";
-
-$(document).ready(function() {
-
-// generates gif buttons
-    var searchButtons = ["husky","westworld","monkey","the office","dogs","subaru","bird","GOT","movies"];
-        for (var i = 0; i < searchButtons.length; i++){
+    for (var i = 0; i <searchButtons.length; i++) {
         var searchBtn = $("<button>");
-        searchBtn.addClass("gif-button");
         searchBtn.attr("data-search", searchButtons[i]);
+        searchBtn.addClass("gif-button");
         searchBtn.text(searchButtons[i]);
           
-        $("#gifSelector").append(searchBtn);
-        buttonListener();
-        };
-
-//search field here to push to searchButtons array
+        $("#gifSelector").append(searchBtn); 
+    }
+}
+// function to add new user button
+function addGifButton () {
     $("#addSearch").on("click", function() {
         var userInput = $("#search-input").val().trim();
        
         searchButtons.push(userInput);
-      
-        
-        $("#search-input").val('');
 
-        //creates new button and adds click event
-        searchBtn = $("<button>");
-        searchBtn.addClass("gif-button");
-        searchBtn.attr("data-search", searchButtons[(searchButtons.length) -1]);
-        searchBtn.text(searchButtons[(searchButtons.length) -1]);
-        
-        $("#gifSelector").append(searchBtn);
-        
-        buttonListener();
-    });   
+        gifButtons();
 
-
-// adding click event for buttons
-    function buttonListener() {    
-    $(".gif-button").on("click", function() {
-        search = $(this).attr("data-search");
-
-        $("#displayGiphy").empty();
-        
-   
+    });
+}
+// function to generate gifs when button is clicked
+function display() {
+    var search = $(this).attr("data-search"); 
+    
     
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    search  + "&api_key=9TDPBQSS97BXCBXaZRIJFKSq1bnBWFpk&limit=10";
-    
+    search  + "&api_key=9TDPBQSS97BXCBXaZRIJFKSq1bnBWFpk&limit=10"; 
+
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         console.log(response);
-    
-    
+
+        $("#displayGiphy").empty();
+
         var results = response.data;
-        
-        //loads static images
-        //some buttons are adding multiple sets of images, from 1-4 times the amount
 
-        for (var i = 0; i < results.length; i++) {
-            // animatedGif = false;
-             
-            var searchDiv = $("<div>");
-    
-            var p = $("<p>").text("Rating: " + results[i].rating);
-    
-            var stillImage = $("<img>");
-            var animateImage = $("<img>");
-            // var imagePosition = [];
-            
-            // searchImage.addClass("gif-button");
-            stillImage.attr("src", results[i].images.fixed_height_still.url);
-            animateImage.attr("src", results[i].images.fixed_height.url);
-            
-            
-            searchDiv.addClass("gif");
-            searchDiv.append(p);
-            searchDiv.append(stillImage);
-            // imagePosition.push(stillImage);
-            // console.log(imagePosition[2]);
-            $("#displayGiphy").prepend(searchDiv);
- 
-          }
-          // all images are animating when clicked, versus only the image clicked
-        
-  
-          $(".gif").on("click", function() {
-       
-            $("#displayGiphy").empty();
-             
-
-        
-          for (var i = 0; i < results.length; i++) {
-            // animatedGif = false;
-            
-            var searchDiv = $("<div>");
-    
-            var p = $("<p>").text("Rating: " + results[i].rating);
-    
-            var stillImage = $("<img>");
-            var animateImage = $("<img>");
-            // searchImage.addClass("gif-button");
-            stillImage.attr("src", results[i].images.fixed_height_still.url);
-            animateImage.attr("src", results[i].images.fixed_height.url);
-           
-            
-            searchDiv.addClass("gif");
-            searchDiv.append(p);
-            searchDiv.append(animateImage);
-            
-            $("#displayGiphy").prepend(searchDiv);
- 
+        for (var i = 0; i < results.length; i++){
           
-        
+            var searchDiv = $("<div>");
+            searchDiv.addClass("searchDiv");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            searchDiv.append(p);
+
+
+            var gifImage = $("<img>");
             
+            gifImage.attr("src", results[i].images.fixed_height_still.url);
+            gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+            gifImage.attr("data-animate", results[i].images.fixed_height.url);
+            gifImage.attr("data-state", "still");
+            gifImage.addClass("gif");
+            
+            
+            searchDiv.addClass("gif");
+            
+            searchDiv.append(gifImage);
+            
+            $("#displayGiphy").prepend(searchDiv);
+ 
 
-            }
-        });
+
+        }
 
 
-    });
-    
+    });    
+}
 
+
+
+
+gifButtons();
+addGifButton();
+
+$(document).on("click", ".gif-button", display);
+
+// toggle to switch between still and animate
+$(document).on("click", ".gif", function(){
+    var state = $(this).attr('data-state');
+    if ( state == 'still'){
+        $(this).attr('src', $(this).data('animate'));
+        $(this).attr('data-state', 'animate');
+    }else{
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
 });
-    };
-
 });
-
 
